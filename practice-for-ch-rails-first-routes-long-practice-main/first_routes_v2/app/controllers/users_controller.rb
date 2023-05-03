9804ebd02e3d101f0e66b0d7a0f9dce54090b04d
vincentpham123 <vincentpham32@gmail.com>
2023-05-03 14:41:34 -0700

    postman tests

---
+++ practice-for-ch-rails-first-routes-long-practice-main/first_routes_v2/app/controllers/users_controller.rb
@@ -0,0 +1,14 @@
+class UsersController < ApplicationController
+
+    def index  
+        render json: params
+    end        
+
+    def create
+        render json: params
+    end
+
+    def show 
+        render json: params
+    end
+end
--- practice-for-ch-rails-first-routes-long-practice-main/first_routes_v2/config/routes.rb
+++ practice-for-ch-rails-first-routes-long-practice-main/first_routes_v2/config/routes.rb
@@ -4,14 +4,14 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  #  resources :users
+   resources :users
 
-  get '/users/', to: 'users#index'
-  get '/users/new', to: 'users#new'
-  post '/users/', to: 'users#create'
-  get '/users/:id', to: 'users#show'
-  get '/users/:id/edit', to: 'users#edit'
-  patch '/users/:id', to: 'users#update'
-  delete '/users/:id', to: 'users#destroy'
+  # get '/users/', to: 'users#index'
+  # get '/users/new', to: 'users#new'
+  # post '/users/', to: 'users#create'
+  # get '/users/:id', to: 'users#show'
+  # get '/users/:id/edit', to: 'users#edit'
+  # patch '/users/:id', to: 'users#update'
+  # delete '/users/:id', to: 'users#destroy'
 
 end
